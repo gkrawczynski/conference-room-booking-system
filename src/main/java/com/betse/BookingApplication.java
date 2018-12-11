2adfc34877afb808f37c11fdc3a15fca28e8bf38
Grzegorz Krawczynski <grzegorzkrawczynski@yahoo.com>
2018-12-11 19:51:40 +0100

    init

---
+++ src/main/java/com/betse/BookingApplication.java
@@ -0,0 +1,13 @@
+package com.betse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class BookingApplication {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(BookingApplication.class, args);
+    }
+}
---
+++ src/main/java/com/betse/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.betse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Hello world!";
+    }
+}
